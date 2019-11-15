778bc561cb2a569a8d374d11bd8a7dff2ffb518f
Tolulomo Fateye <tolulomofateye@gmail.com>
2019-11-15 17:49:15 -0500

    First Commit

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,1 @@
+alert("Hello this is just a text");
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+module.exports = {
+    entry: './app/assets/scripts/App.js',
+    output: {
+        filename: 'bundled.js',
+        path: path.resolve(__dirname, 'app')
+    },
+    mode: 'development'
+}
