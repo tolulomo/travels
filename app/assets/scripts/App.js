2268d4a871edbb8d10d0751fcb4a3f0760ff9783
Tolulomo Fateye <tolulomofateye@gmail.com>
2019-12-01 22:54:59 -0500

    Adding mobile icon

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,7 @@
 import '../styles/style.css'
+import MobileMenu from './jsmodules/MobileMenu';
+
+const Menu = new MobileMenu()
 
 if(module.hot){
     module.hot.accept();
---
+++ app/assets/scripts/jsmodules/MobileMenu.js
@@ -0,0 +1,20 @@
+class MobileMenu {
+    constructor() {
+        this.icon = document.querySelector(".navigation__mobile-icon");
+        this.menuContainer = document.querySelector(".page-section__header-container");
+        this.headerContainer = document.querySelector(".page-section__header");
+        this.onClickEvent();
+    }
+    
+    onClickEvent(){
+        this.icon.addEventListener("click", ()=>this.toggleMenu());
+    };
+
+    toggleMenu(){
+        this.headerContainer.classList.toggle("page-section__header--mobile-menu-bg");
+        this.menuContainer.classList.toggle("page-section__header-toggle-menu");
+    }
+}
+
+
+export default MobileMenu;
